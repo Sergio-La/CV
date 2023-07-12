3d78d7b77f32bc4e1b36da1932c6c34a70c5f5e4
Sergio-La <selui2100@gmail.com>
2023-07-12 07:15:22 -0500

    se trabajó en los estilos de los botones y se incluyó contenido en main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const btnMode = document.querySelector(".mode");
+const btnPortfolio = document.querySelector("#portfolio");
+const btnSkills = document.querySelector("#skills");
+const proyectsSkillsFlex = document.querySelector(".proyects-skills-flex");
+
+btnMode.addEventListener("click", toggleMode);
+btnPortfolio.addEventListener("click", seePortfolio);
+btnSkills.addEventListener("click", seeSkills);
+
+function toggleMode() {
+  console.log("click");
+}
+
+function seePortfolio() {
+  console.log("click");
+}
+
+function seeSkills() {
+  console.log("click");
+}
