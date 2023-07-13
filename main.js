c12d69c7a75d4f64607d933f74bb444a929b8a0a
Sergio-La <selui2100@gmail.com>
2023-07-12 21:54:04 -0500

    en main.js se cambió el selector del modo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const btnMode = document.querySelector(".mode");
+const btnMode = document.querySelector("#mode");
 const btnPortfolio = document.querySelector("#portfolio");
 const btnSkills = document.querySelector("#skills");
 const proyectsSkillsFlex = document.querySelector(".proyects-skills-flex");
