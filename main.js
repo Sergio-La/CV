441443819c94fbf9a42ada8c66de6b61f852fde8
Sergio-La <selui2100@gmail.com>
2023-07-16 21:57:01 -0500

    se agregaron una platilla de ejemplo de los proyectos y algunas clases del modo oscuro

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
+const body = document.body;
+/*checkbox mode*/
 const btnMode = document.querySelector("#mode");
+/*texto con el nombre*/
+const textName = document.querySelector(".name");
+/*texto con el nombre*/
+const textTitle = document.querySelector(".title");
+/*botón del portafolio*/
 const btnPortfolio = document.querySelector("#portfolio");
+/*botón de las skills*/
 const btnSkills = document.querySelector("#skills");
-const proyectsSkillsFlex = document.querySelector(".proyects-skills-flex");
+/*sección del protafolio*/
+const projectsSkillsFlex = document.querySelector(".projects-skills-flex");
 
 btnMode.addEventListener("click", toggleMode);
 btnPortfolio.addEventListener("click", seePortfolio);
 btnSkills.addEventListener("click", seeSkills);
 
+/*Función para el selector de modo*/
 function toggleMode() {
-  console.log("click");
+  body.classList.toggle('body-dark');
+  textName.classList.toggle('name-dark');
+  textTitle.classList.toggle('title-dark');
 }
 
 function seePortfolio() {
